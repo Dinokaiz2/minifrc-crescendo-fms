04f263f298a9ef081eba4469fe1511478a1fa434
kakhtar <kakhtar@ncsu.edu>
2023-07-15 09:22:03 -0400

    Fix ranking tiebreakers

--- src/renderer/controller.js
+++ src/renderer/controller.js
@@ -187,9 +187,9 @@ export class Competition {
         teams.forEach(team => team.calculateFields())
         teams.sort((t1, t2) => {
             if (t1.rankingScore != t2.rankingScore) return t2.rankingScore - t1.rankingScore;
-            if (t1.autoPoints != t2.cargoPoints) return t2.cargoPoints - t1.cargoPoints;
-            if (t1.endgamePoints != t2.endgamePoints) return t2.endgamePoints - t1.endgamePoints;
-            return t2.boulderPoints - t1.boulderPoints;
+            if (t1.autoPoints != t2.autoPoints) return t2.autoPoints - t1.autoPoints;
+            if (t1.chargeParkPoints != t2.chargeParkPoints) return t2.chargeParkPoints - t1.chargeParkPoints;
+            return t2.gridPoints - t1.gridPoints;
         });
         return teams;
     }
