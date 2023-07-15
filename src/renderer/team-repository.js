fa2acd50d45ca350de2d879fddfb89817b31bfd3
kakhtar <kakhtar@ncsu.edu>
2023-07-15 09:25:18 -0400

    Clean team generate fn

--- src/renderer/team-repository.js
+++ src/renderer/team-repository.js
@@ -10,28 +10,6 @@ import { Team } from "./team.js"
 
 // Put manual db generation here
 function generate() {
-    generateTeam(1, "");
-    generateTeam(2, "");
-    generateTeam(3, "");
-    generateTeam(4, "");
-    generateTeam(5, "");
-    generateTeam(6, "");
-    generateTeam(7, "");
-    generateTeam(8, "");
-    generateTeam(9, "");
-    generateTeam(10, "");
-    generateTeam(11, "");
-    generateTeam(12, "");
-    generateTeam(13, "");
-    generateTeam(14, "");
-    generateTeam(15, "");
-    generateTeam(16, "");
-    generateTeam(17, "");
-    generateTeam(18, "");
-    generateTeam(19, "");
-    generateTeam(20, "");
-    generateTeam(21, "");
-    generateTeam(22, "");
 }
 
 $(generate);
